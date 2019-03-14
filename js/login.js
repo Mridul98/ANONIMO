e5a4c9c61ae1fb58d86c43dae564b0dfd5a0f6d9
Mahmud un nabi <munmridul@gmail.com>
2019-03-14 16:09:15 +0600

    login and registration page comolete

---
+++ js/login.js
@@ -0,0 +1,12 @@
+
+window.onload = function(){
+    var clicked = false;
+    const create = document.getElementById("create");
+    create.onclick = function(){
+        clicked = true;
+        location.href = "/html/register.html";
+        
+    }
+    
+    
+}
---
+++ js/register.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    var clicked = false;
+    const create = document.getElementById("submitButton");
+    create.onclick = function(){
+        clicked = true;
+        location.href = "/html/login.html";
+        
+    }
+    
+    
+}
