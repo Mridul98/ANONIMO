1dcc67a0d55abf9268b5002924f9131d5453b044
Mahmud un nabi <munmridul@gmail.com>
2019-03-21 13:19:54 +0600

    added registration.php

---
+++ php/registration.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])){
+     
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    echo "".$email.$username.$password;
+
+}
+
+
+
+?>
