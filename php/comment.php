a74b9a1bf9c56f4be1410fd3e4a6b307b73fef0b
Mahmud un nabi <munmridul@gmail.com>
2019-03-28 20:02:50 +0600

    added some javascripts in mainpage.html

---
+++ php/comment.php
@@ -0,0 +1,12 @@
+<?php
+
+
+if(isset($_POST['tweet'])){
+    $tweet = $_POST['tweet'];
+    echo $tweet;
+}
+ else echo "no tweet";
+
+
+
+?>
--- php/continue.php
+++ php/continue.php
@@ -1,23 +1,47 @@
 <?php
 require_once 'access.php';
+class Person{
+     public $user , $messages;
+}
+$person = new Person;
+
+$tweets = array();
+
 session_start();
 if(isset($_SESSION['username'])){
      $ml = $_SESSION['username'];
-     $connection = $connection = new mysqli($host,$username,'',$db);
-     if($connection->connect_error == false){
-         $query = "INSERT INTO tweets (user,message) values('$ml','you are very good')";
 
-         if($connection->query($query)===TRUE) {echo "success";}
-               else echo $connection->error;
+     $connection = $connection = new mysqli($host,$username,'',$db);
+    
+     if($connection->connect_error == false){
+         $query = "SELECT message FROM tweets WHERE user = '$ml'";
+
+         $result  = $connection->query($query);
+         if($result->num_rows>0){
+              while($rows = $result->fetch_assoc()){
+                   array_push($tweets,$rows['message']);
+                   
+              }
+              $person->user = $ml;
+              $person->messages = $tweets;
+              //header('location:http://localhost/anonimo/html/mainpage.html');
+              echo json_encode($person);
+              
+         }
+
+         $connection->close();
+               
 
      } 
 
 }
-echo session_id();
-unset($_SESSION['username']);
-session_destroy();
-echo session_id();
+//echo session_id();
+//unset($_SESSION['username']);
+//session_destroy();
+//
 //header('location:http://localhost/anonimo/html/mainpage.html');
+
+
 
 
 
