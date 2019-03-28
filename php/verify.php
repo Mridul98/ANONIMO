3af0e9070db75fe0f84ac959228989425ce8596b
Mahmud un nabi <munmridul@gmail.com>
2019-03-28 10:50:24 +0600

    modified verify.php

---
+++ php/verify.php
@@ -0,0 +1,31 @@
+<?php
+require 'access.php';
+if(isset($_POST['username']) && isset($_POST['password'])){
+$username = $_POST['username'];
+$password = $_POST['password'];
+$query = "SELECT Password FROM user WHERE Username = '$username'  ";
+$connection = new mysqli($host,'root','',$db);
+if($connection->connect_error== false){
+    $passRes = $connection->query($query);
+
+    if($passRes->num_rows >0){
+        while($row = $passRes->fetch_assoc()){
+            if(password_verify($password,$row['Password'])){
+                session_start();
+                $_SESSION['username'] = $username;
+                
+                echo "session started";
+                header('location:http://localhost/anonimo/html/mainpage.html');
+
+        }
+        else echo "YOU DIDNT REGISTER YET!";
+    }
+}
+} else echo "connection error";
+
+} else echo "username and password not set";
+
+
+
+
+?>
