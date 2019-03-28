22554b69c4044089afab2ad6300417632785605f
Mahmud un nabi <munmridul@gmail.com>
2019-03-28 12:25:24 +0600

    dropped primary key of table tweets

--- php/continue.php
+++ php/continue.php
@@ -1,12 +1,22 @@
 <?php
+require_once 'access.php';
 session_start();
 if(isset($_SESSION['username'])){
-     $_SESSION['username'];
+     $ml = $_SESSION['username'];
+     $connection = $connection = new mysqli($host,$username,'',$db);
+     if($connection->connect_error == false){
+         $query = "INSERT INTO tweets (user,message) values('$ml','you are very good')";
+
+         if($connection->query($query)===TRUE) {echo "success";}
+               else echo $connection->error;
+
+     } 
+
 }
-
-
+echo session_id();
+unset($_SESSION['username']);
 session_destroy();
-echo "session_destroyed";
+echo session_id();
 //header('location:http://localhost/anonimo/html/mainpage.html');
 
 
